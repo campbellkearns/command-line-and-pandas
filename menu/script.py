166678ec6d70d890370c9e0aad284284c95c9801
Brandon Campbell-Kearns <brandon.j.kearns@gmail.com>
2025-11-18 20:06:24 -0500

    Adds invalid count logic

--- menu/script.py
+++ menu/script.py
@@ -1,8 +1,11 @@
 def main_menu():
+    # keep track of invalid inputs
+    invalid_count = 0
+
     while True:
         print("\n--- Main Menu ---")
         print("1.Greet User")
-        print("2. Show Info")
+        print("2. Show Data Summary")
         print("3. Exit")
 
         choice = input("Enter your choice (1-3)")
@@ -15,6 +18,10 @@ def main_menu():
             print("Goodbye.")
             break
         else:
-            print("Please enter 1, 2, or 3")
-
+            print("Please select 1, 2, or 3")
+            invalid_count += 1
+            if invalid_count >= 3:
+                print("Too many invalid attempts. Start over.")
+                break
+        
 main_menu()
